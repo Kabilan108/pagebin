594a68a9bc4da31ba2f680bfaa31234dc52598dc
Tony Okeke <tonykabilanokeke@gmail.com>
2026-07-16 09:47:52 -0400

    Make release retries safe and reproducible
    - vendor locked Markdown runtime dependencies into the Nix package build
    - publish when the current package release is absent, independent of the latest version diff
    - serialize release runs, target the triggering commit, and never overwrite existing assets

--- flake.nix
+++ flake.nix
@@ -16,6 +16,26 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
+          runtimeDependencies = {
+            argparse = pkgs.fetchzip {
+              url = "https://registry.npmjs.org/argparse/-/argparse-2.0.1.tgz";
+              sha256 = "1dv896y3piqdv70w06fyj0pp6pm6qdbba13sy78n3jywl4jfizps";
+            };
+            highlightJs = pkgs.fetchzip {
+              url = "https://registry.npmjs.org/highlight.js/-/highlight.js-${
+                packageJson.dependencies."highlight.js"
+              }.tgz";
+              sha256 = "1c3cy1nq1in2h3rbnyla26fwm1im5fd78fv1d9bbpnm59l7n8www";
+            };
+            jsYaml = pkgs.fetchzip {
+              url = "https://registry.npmjs.org/js-yaml/-/js-yaml-${packageJson.dependencies."js-yaml"}.tgz";
+              sha256 = "1pzkq1qivm58hv0ij1vhsrjrmahqb36f6yzbspi3ppwwik66ksm6";
+            };
+            marked = pkgs.fetchzip {
+              url = "https://registry.npmjs.org/marked/-/marked-${packageJson.dependencies.marked}.tgz";
+              sha256 = "0jd33sksb2r016dq24ddxqc7jdjv8b05imfw3vj615mpkl2df6r3";
+            };
+          };
         in
         rec {
           pagebin = pkgs.stdenvNoCC.mkDerivation {
@@ -36,6 +56,11 @@
 
             buildPhase = ''
               runHook preBuild
+              mkdir -p node_modules
+              cp -R ${runtimeDependencies.argparse} node_modules/argparse
+              cp -R ${runtimeDependencies.highlightJs} node_modules/highlight.js
+              cp -R ${runtimeDependencies.jsYaml} node_modules/js-yaml
+              cp -R ${runtimeDependencies.marked} node_modules/marked
               bun build ./src/cli.ts --compile --target=bun-linux-x64 --outfile pagebin
               runHook postBuild
             '';
